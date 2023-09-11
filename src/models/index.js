d45b0f58ec25f8aec519b4b4090d0f022e5e1c0e
nico <nico.bernard2207@gmail.com>
2023-09-11 20:03:16 -0300

    movie app back-end

---
+++ src/models/index.js
@@ -0,0 +1,14 @@
+const Movies = require('./Movies');
+const Genres = require("./Genres");
+const Actors = require("./Actors");
+const Directors = require('./Directors');
+
+
+Movies.belongsToMany(Genres,{through:"MoviesGenres"});
+Genres.belongsToMany(Movies,{through:"MoviesGenres"});
+
+Movies.belongsToMany(Actors,{through:"MoviesActors"});
+Actors.belongsToMany(Movies,{through:"MoviesActors"});
+
+Movies.belongsToMany(Directors,{through:"MoviesDirectors"});
+Directors.belongsToMany(Movies,{through:"MoviesDirectors"});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const genreRouter = require('./genre.router');
+const moviesRouter = require('./movies.router');
+const actorRouter = require('./actor.router');
+const directorRouter = require('./director.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
+router.use('/genres',genreRouter)
+router.use('/movies',moviesRouter)
+router.use('/actors',actorRouter)
+router.use('/directors',directorRouter)
 
 module.exports = router;
